d6a44e59a8c52893422efdee381dd0f68ccee0f8
Cliff Choiniere <crcnum4@gmail.com>
2020-04-26 21:02:38 -0400

    added auth actions for registration and login

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const auth = require("./routes/auth");
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", auth);
+
 const port = process.env.PORT || 6000;
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
+
+// gateway >> auth service
+// frontend >> gateway >> authservice
+
+// Post /api/auth
+// post /api/posts
+// post /api/comrev
+
+/*
+  {
+    action: 'keyword ex: registerUser, login, addProfile, getSelf',
+    reqBody: {
+      body
+    }
+  }
+
+  auth token still sent via x-auth-token header key
+*/
